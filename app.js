7f1f17afbef4be8df4f4120a0f786248480d79a5
rubenssi <rubenssi825@gmail.com>
2021-10-21 00:34:49 -0300

    create app.js commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*') // permite conexões de qualquer origen
+  res.header('Access-Control-Allow-Origin', '*') // permite conexões de qualquer origem
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE') // especifica que tipos de métodos
   res.header('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type, Authorization')
   app.use(cors())
